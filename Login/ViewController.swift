038c6bb4f32c31a0306e91deec2b67d49db26ab6
Builderman4 <ibrahim.syed5612@gmail.com>
2023-02-09 19:10:42 -0500

    commit

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -21,8 +21,32 @@ class ViewController: UIViewController {
         
     }
     
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    @IBOutlet var forgotPassword: UIButton!
+    
+    @IBOutlet var forgotUsername: UIButton!
+    
+    @IBAction func forgotUsername(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+    @IBAction func forgotPassword(unwindSegue: UIStoryboardSegue) {
+    }
+    
+  
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
      segue.destination.navigationItem.title = usernameTextField.text
+        
+        guard let sender = sender as? UIButton else {return}
+         
+          if sender == forgotPassword {
+               segue.destination.navigationItem.title = "Forgot Password"
+         } else if sender == forgotUsername {
+               segue.destination.navigationItem.title = "Forgot Username"
+         } else {
+            segue.destination.navigationItem.title = usernameTextField.text
+         }
         
     }
     
